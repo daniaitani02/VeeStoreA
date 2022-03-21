882e59f60c3a8901247e1225f317d0c309ab564a
Ahmad Safari <60101938@cna-qatar.edu.qa>
2022-03-21 22:58:52 +0300

    added admin panel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using VeeStoreA.Models;
 
 namespace VeeStoreA.Controllers
 {
     public class HomeController : Controller
     {
+        private VeeStoreDbEntities db = new VeeStoreDbEntities();
         public ActionResult Index()
         {
            
@@ -17,6 +21,18 @@ namespace VeeStoreA.Controllers
             return View();
         }
 
+        public ActionResult AdminPanel()
+        {
+
+            if(User.Identity.Name != "admin@admin.com") return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            ViewBag.totalCustomers = db.Customers.Count();
+            ViewBag.totalProducts = db.Products.Count();
+            ViewBag.totalEarnings = db.Carts.Where(c => c.Status == "Paid").Sum(c => c.CartItems.Sum(item => item.Quantity * item.Product.Price));
+            ViewBag.totalPaidCarts = db.Carts.Where(c => c.Status == "Paid").Count();
+
+
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
