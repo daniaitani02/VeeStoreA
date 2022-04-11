a97e371e6e33ffb43c1a3a1ef9cc1e2e006c9a14
Ahmad Safari <60101938@cna-qatar.edu.qa>
2022-04-11 13:31:30 +0300

    fixed permission mistake

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -34,10 +34,10 @@ namespace VeeStoreA.Controllers
             {
                 return HttpNotFound();
             }
-            if (User.Identity.Name == "admin@admin.com") return View(customer);
+           
 
             // Does the customer object belong to the logged in user? If not, return Forbidden error
-            if (customer.Email != User.Identity.Name) return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            if (customer.Email != User.Identity.Name && User.Identity.Name != "admin@admin.com") return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
             ViewBag.CurrencyId = new SelectList(db.Currencies, "Id", "ShortName", customer.CurrencyId);
             ViewBag.Type = new SelectList(new List<string> { "Visa", "Mastercard", "American Express" }, "Visa");
 
@@ -46,29 +46,7 @@ namespace VeeStoreA.Controllers
 
         
 
-        // GET: Customers/Edit/5
-        //public ActionResult Edit(string id)
-        //{
-        //    ViewBag.CurrencyId = new SelectList(db.Currencies, "Id", "ShortName",null);
 
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Customer customer = db.Customers.Find(id);
-        //    if (customer == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    // Does the customer object belong to the logged in user? If not, return Forbidden error
-        //    if (customer.Email != User.Identity.Name) return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
-
-        //    return View(customer);
-        //}
-
-        // POST: Customers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Email,Name,CurrencyId,Gender,JoinedAt,Status,PhoneNumber")] Customer customer)
@@ -119,31 +97,7 @@ namespace VeeStoreA.Controllers
 
             return View(orders.ToList());
         }
-        // GET: Customers/Delete/5
-        //public ActionResult Delete(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Customer customer = db.Customers.Find(id);
-        //    if (customer == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(customer);
-        //}
-
-        //// POST: Customers/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(string id)
-        //{
-        //    Customer customer = db.Customers.Find(id);
-        //    db.Customers.Remove(customer);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
+       
 
         protected override void Dispose(bool disposing)
         {
