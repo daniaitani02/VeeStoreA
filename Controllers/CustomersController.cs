1f2f489445df60ca005d1eb53cfd6d06b740dfaf
Ahmad Safari <60101938@cna-qatar.edu.qa>
2022-04-12 17:04:34 +0300

    seperated credit cards from profile to allow for validation

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -37,6 +37,13 @@ namespace VeeStoreA.Controllers
 
             // Does the customer object belong to the logged in user? If not, return Forbidden error
             if (customer.Email != User.Identity.Name && User.Identity.Name != "admin@admin.com") return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            //if (creditCard != null){
+            //    ViewBag.CreditCard = creditCard;
+            //}
+            //else{
+            //    ViewBag.CreditCard = new CreditCard { };
+            //}
+        
             ViewBag.CurrencyId = new SelectList(db.Currencies, "Id", "ShortName", customer.CurrencyId);
             ViewBag.Type = new SelectList(new List<string> { "Visa", "Mastercard", "American Express" }, "Visa");
 
@@ -83,20 +90,55 @@ namespace VeeStoreA.Controllers
             return RedirectToAction("Details", new { id = customer.Email + "/" });
         }
 
-        public ActionResult AddCreditCard([Bind(Include = "Name,Number,CVV,Expiry")] CreditCard creditCard,string Type)
+        public ActionResult CreateCreditCard(string id)
+
         {
-            creditCard.CustomerEmail = User.Identity.Name;
-            creditCard.Type = Type;
-            db.CreditCards.Add(creditCard);
-            db.SaveChanges();
-            return RedirectToAction("Details",new { id=User.Identity.Name +"/"});
+            string CustomerEmail = id;
+            if (CustomerEmail == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(CustomerEmail);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Does the customer object belong to the logged in user? If not, return Forbidden error
+            if (customer.Email != User.Identity.Name && User.Identity.Name != "admin@admin.com") return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            if(customer.CreditCards.Where(cc => cc.Type != "Disabled").Count() != 0)
+            {
+                ViewBag.creditCards = customer.CreditCards.Where(cc => cc.Type != "Disabled");
+            }
+            
+            ViewBag.Type = new SelectList(new List<string> { "Visa", "Mastercard", "American Express" }, "Visa");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCreditCard([Bind(Include = "Name,Number,CVV,Expiry")] CreditCard creditCard,string Type)
+        {
+            ViewBag.Type = new SelectList(new List<string> { "Visa", "Mastercard", "American Express" }, "Visa");
+            if (ModelState.IsValid)
+            {
+                creditCard.CustomerEmail = User.Identity.Name;
+                creditCard.Type = Type;
+                db.CreditCards.Add(creditCard);
+                db.SaveChanges();
+                Customer customer = db.Customers.Find(User.Identity.Name);
+                if (customer.CreditCards.Where(cc => cc.Type != "Disabled").Count() != 0)
+                {
+                    ViewBag.creditCards = customer.CreditCards.Where(cc => cc.Type != "Disabled");
+                }
+                return View();
+            }
+            return View(creditCard);
         }
         public ActionResult RemoveCreditCard(int? CardId)
         {
             CreditCard creditCard = db.CreditCards.Find(CardId);
             creditCard.Type = "Disabled";
             db.SaveChanges();
-            return RedirectToAction("Details", "Customers", new { id = User.Identity.Name +"/" });
+            return RedirectToAction("CreateCreditCard", "Customers", new { id = User.Identity.Name +"/" });
         }
         public ActionResult Orders(string id)
         {
